9d0dd068b34531698ae8174c224884ca0dc714ee
refuzed <refuzed@gmail.com>
2013-03-28 01:37:00 -0500

    Implemented xbox controls

--- Assets/EarlyFlightModel.cs
+++ Assets/EarlyFlightModel.cs
@@ -3,56 +3,92 @@ using System.Collections;
 
 public class EarlyFlightModel : MonoBehaviour {
 	
+	public float deadzone;
+	public float lookSensitivity;
 	public float speed;
 	public float forceMod;
 	public float turnSpeed;
-	private Vector3 lastPosition;
 	
 	void Start () {
+		deadzone = 0.5f;
+		lookSensitivity = 5.0f;
 		forceMod = 15.0f;
 		turnSpeed = 0.3f;
-		lastPosition = Vector3.zero;
 	}
 	
 	void FixedUpdate () {
-        speed = (transform.position - lastPosition).magnitude;
-        lastPosition = transform.position;
-		
+		CheckKeyboardInputs();
+		CheckGamepadInputs();
+	}
+	
+	void CheckKeyboardInputs()
+	{
 		// THRUST
         if(Input.GetKey(KeyCode.Space)) // VTOL
-            ApplyLinearForce(transform.up);
+            ApplyLinearForce(transform.up, 1);
 		if(Input.GetKey(KeyCode.UpArrow)) // FORWARD
-            ApplyLinearForce(transform.forward);
+            ApplyLinearForce(transform.forward, 1);
 		if(Input.GetKey(KeyCode.DownArrow)) // REVERSE
-            ApplyLinearForce(-transform.forward);
+            ApplyLinearForce(-transform.forward, 1);
 		
 		// PITCH
         if(Input.GetKey(KeyCode.W)) // PITCH DOWN
-            ApplyRotationalForce(transform.right);
+            ApplyRotationalForce(transform.right, 1);
         if(Input.GetKey(KeyCode.S)) // PITCH UP
-            ApplyRotationalForce(-transform.right);
+            ApplyRotationalForce(-transform.right, 1);
 		
 		// YAW
-        if(Input.GetKey(KeyCode.LeftArrow)) //YAW LEFT
-            ApplyRotationalForce(-transform.up);
         if(Input.GetKey(KeyCode.RightArrow)) //YAW RIGHT
-            ApplyRotationalForce(transform.up);
+            ApplyRotationalForce(transform.up, 1);
+        if(Input.GetKey(KeyCode.LeftArrow))  //YAW LEFT
+            ApplyRotationalForce(transform.up, -1);
 		
 		// ROTATION
-		if(Input.GetKey (KeyCode.A)) //ROTATE LEFT
-			ApplyRotationalForce(transform.forward);
 		if(Input.GetKey (KeyCode.D)) //ROTATE RIGHT
-			ApplyRotationalForce(-transform.forward);
+			ApplyRotationalForce(transform.forward, -1);
+		if(Input.GetKey (KeyCode.A)) //ROTATE LEFT
+			ApplyRotationalForce(transform.forward, 1);
 	}
 	
-	
-	void ApplyLinearForce(Vector3 direction)
+	void CheckGamepadInputs()
 	{
-		rigidbody.AddForce(direction * forceMod);
+		// THRUST
+        if(Input.GetAxis("LeftVertical") > deadzone) // VTOL
+            ApplyLinearForce(transform.up, Input.GetAxis("LeftVertical") * 2.0f);
+        if(Input.GetAxis("LeftVertical") < -deadzone) // INVERSE VTOL
+            ApplyLinearForce(transform.up, Input.GetAxis("LeftVertical"));
+		if(Input.GetButton("LB")) // FORWARD THRUST
+            ApplyLinearForce(transform.forward, -1);
+        if(Input.GetButton("RB")) // REVERSE THRUST
+            ApplyLinearForce(transform.forward, 1);
+		
+		// PITCH
+        if(Input.GetAxis("RightVertical") > deadzone) // PITCH DOWN
+            ApplyRotationalForce(transform.right, Input.GetAxis("RightVertical") * lookSensitivity);
+        if(Input.GetAxis("RightVertical") < -deadzone) // PITCH UP
+            ApplyRotationalForce(transform.right, Input.GetAxis("RightVertical") * lookSensitivity);
+		
+		// YAW
+        if(Input.GetAxis("LeftHorizontal") > deadzone) //YAW RIGHT
+            ApplyRotationalForce(transform.up, Input.GetAxis("LeftHorizontal") * lookSensitivity / 3);
+        if(Input.GetAxis("LeftHorizontal") < -deadzone) //YAW LEFT
+            ApplyRotationalForce(transform.up, Input.GetAxis("LeftHorizontal") * lookSensitivity / 3);
+
+		
+		// ROTATION
+		if(Input.GetAxis("RightHorizontal") > deadzone) //ROTATE RIGHT
+			ApplyRotationalForce(transform.forward, -Input.GetAxis("RightHorizontal") * lookSensitivity);	
+		if(Input.GetAxis("RightHorizontal") < -deadzone) //ROTATE LEFT
+			ApplyRotationalForce(transform.forward, -Input.GetAxis("RightHorizontal") * lookSensitivity);
 	}
 	
-	void ApplyRotationalForce(Vector3 direction)
+	void ApplyLinearForce(Vector3 direction, float extraIntensity)
 	{
-		rigidbody.AddTorque(direction * turnSpeed);
+		rigidbody.AddForce(direction * forceMod * extraIntensity);
+	}
+	
+	void ApplyRotationalForce(Vector3 direction, float extraIntensity)
+	{
+		rigidbody.AddTorque(direction * turnSpeed * extraIntensity);
 	}
 }
--- Assets/Gui2ui.cs
+++ Assets/Gui2ui.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class Gui2ui : MonoBehaviour {
 	
-	public float speed = 0;
 	public GUIStyle customGuiStyle;
 	
 	void OnGUI(){
 		
-		EarlyFlightModel target = transform.parent.gameObject.GetComponent<EarlyFlightModel>();
-		GUI.Box (new Rect (0,0,200,50), "Relative Speed: " + Mathf.Round(target.speed*10), customGuiStyle);
-
+		//EarlyFlightModel target = transform.parent.gameObject.GetComponent<EarlyFlightModel>();
+		//GUI.Box (new Rect (0,0,200,50), "Left Horizontal: " + Input.GetAxis ("LeftHorizontal"), customGuiStyle);
+		//GUI.Box (new Rect (0,20,200,50), "Left Vertical: " + Input.GetAxis ("LeftVertical"), customGuiStyle);
+		//GUI.Box (new Rect (0,40,200,50), "Right Horizontal: " + Input.GetAxis ("RightHorizontal"), customGuiStyle);
+		//GUI.Box (new Rect (0,60,200,50), "Right Vertical: " + Input.GetAxis ("RightVertical"), customGuiStyle);
+		GUI.Box (new Rect (0,0,200,50), "Fire1: " + Input.GetButton("Fire1"), customGuiStyle);
+		GUI.Box (new Rect (0,20,200,50), "Fire2: " + Input.GetButton("Fire2"), customGuiStyle);
+		GUI.Box (new Rect (0,40,200,50), "Fire3: " + Input.GetButton("Fire3"), customGuiStyle);
+		GUI.Box (new Rect (0,60,200,50), "Fire4: " + Input.GetButton("Fire4"), customGuiStyle);
+		GUI.Box (new Rect (0,80,200,50), "Fire5: " + Input.GetButton("Fire5"), customGuiStyle);
+		GUI.Box (new Rect (0,100,200,50), "Fire6: " + Input.GetButton("Fire6"), customGuiStyle);
+		GUI.Box (new Rect (0,120,200,50), "Fire7: " + Input.GetButton("Fire7"), customGuiStyle);
 	}
 
 }
